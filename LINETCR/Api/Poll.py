036c3854748ae6737d73acc0c50b2864f09da232
Aldiergokil <33947049+Aldiergokil@users.noreply.github.com>
2018-01-17 00:02:03 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,7 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/1.4.17"
+#  UA = "Line/1.4.17"
+   UA = "Line/7.18.0"
 #  LA = "IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0"
 #  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
   LA = "IOSIPAD\t7.18.0\tiPhone OS\t11.12.1"
